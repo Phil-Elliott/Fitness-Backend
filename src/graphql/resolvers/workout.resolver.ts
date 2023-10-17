3a973380dda7b5e6c9889b9a21464b5d3e7d3ae4
Phil <phillippelliott@gmail.com>
2023-10-17 15:19:57 +0800

    fixed bugs with workout handlers

--- src/graphql/resolvers/workout.resolver.ts
+++ src/graphql/resolvers/workout.resolver.ts
@@ -4,7 +4,7 @@ const WorkoutResolvers = {
   Query: {
     // Get a single Workout by ID
     workout: async (_: any, args: { id: number }) => {
-      return await prisma.workout.findUnique({
+      const workout = await prisma.workout.findUnique({
         where: {
           id: args.id,
         },
@@ -12,11 +12,21 @@ const WorkoutResolvers = {
           exercises: true, // Include related exercises
         },
       });
+
+      if (!workout) return null; // handle case where workout is not found
+
+      const formattedDate = workout.date.toISOString();
+
+      // Return a new object with the formatted date and spread in other workout properties
+      return {
+        ...workout,
+        date: formattedDate,
+      };
     },
 
     // Get all Workouts of a specific User
     userWorkouts: async (_: any, args: { userId: number }) => {
-      return await prisma.workout.findMany({
+      const workouts = await prisma.workout.findMany({
         where: {
           userId: args.userId,
         },
@@ -24,28 +34,34 @@ const WorkoutResolvers = {
           exercises: true, // Include related exercises
         },
       });
+
+      // Map over the workouts array and transform the date for each workout
+      return workouts.map((workout) => ({
+        ...workout,
+        date: workout.date.toISOString(),
+      }));
     },
   },
 
   Mutation: {
     // Create a new Workout for a User
-    createWorkout: async (_: any, args: { userId: number; date?: Date }) => {
+    createWorkout: async (_: any, args: { userId: number; date?: string }) => {
       return await prisma.workout.create({
         data: {
           userId: args.userId,
-          date: args.date || new Date(), // Use provided date or default to current date
+          date: args.date ? new Date(args.date) : new Date(), // Convert string to Date object
         },
       });
     },
 
     // Update a Workout by ID (e.g., to change the date)
-    updateWorkout: async (_: any, args: { id: number; date: Date }) => {
+    updateWorkout: async (_: any, args: { id: number; date: string }) => {
       return await prisma.workout.update({
         where: {
           id: args.id,
         },
         data: {
-          date: args.date,
+          date: new Date(args.date), // Convert string to Date object
         },
       });
     },
